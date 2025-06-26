a7fdb3be6f6020b733301d8b4ffc5f7284acca4b
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-06-26 18:10:06 +0300

    Реализация класса сервера в рамках второго задания. Добавление тестов для проверки функции GetCount и AddToCount.

---
+++ CleverenceTestTask.Test/ServerTest.cs
@@ -0,0 +1,62 @@
+﻿using CleverenceTestTask.Задание_2;
+
+namespace CleverenceTestTask.Test
+{
+    [TestClass]
+    public class ServerTest
+    {
+        [TestMethod]
+        public void GetCount_0_0returned()
+        {
+            Server.Clear();
+            int expected = 0;
+
+            int actual = Server.GetCount();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddToCount_100_100returned()
+        {
+            Server.Clear();
+            int count = 100;
+            int expected = 100;
+
+            Server.AddToCount(count);
+
+            int actual = Server.GetCount();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddToCount_100_20_120returned()
+        {
+            Server.Clear();
+            int count1 = 100;
+            int count2 = 20;
+            int expected = 120;
+
+            Server.AddToCount(count1);
+            Server.AddToCount(count2);
+
+            int actual = Server.GetCount();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetCountParallel_0_0returned()
+        {
+            Server.Clear();
+            int r1 = 0, r2 = 0;
+            Parallel.Invoke(
+                () => r1 = Server.GetCount(),
+                () => r2 = Server.GetCount()
+            );
+
+            Assert.AreEqual(r1, r2);
+        }
+    }
+}
---
+++ CleverenceTestTask/Задание 2/Server.cs
@@ -0,0 +1,49 @@
+﻿namespace CleverenceTestTask.Задание_2
+{
+    public static class Server
+    {
+        private static int _count = 0;
+        private static readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+        public static int GetCount()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _count;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        public static void AddToCount(int count)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _count += count;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        #if DEBUG
+        public static void Clear()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _count = 0;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+        #endif
+    }
+}
